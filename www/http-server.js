a68a38f0015d631ebc38787a5bb5819fda5d3f0a
yayan_bachtiar <abiedoank@gmail.com>
2016-04-18 09:37:14 +0700

    add grunt file, add live reload, add npm

---
+++ www/http-server.js
@@ -0,0 +1,18 @@
+var http = require('http'),
+    PORT = 3400;
+
+var server = http.createServer(function(req, res){
+    var body = "<pre>Haruskah belajar Node.js?</pre><p><h3>...Yo Mesto!</h3></p>"
+    res.writeHead(200, {
+        'Content-Length':body.length,
+        'Content-Type':'text/html',
+        'Pesan-Header':'Pengenalan Node.js'
+    });
+
+    res.write(body);
+    res.end();
+});
+
+server.listen(PORT);
+
+console.log("Port "+PORT+" : Node.js Server...");
